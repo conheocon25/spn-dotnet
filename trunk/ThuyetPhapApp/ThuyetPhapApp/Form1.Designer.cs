8bb1680b38dd67ff1dd4ace0ef49ea064f6321b7
tuanbuithanh <tuanbuithanh@2ff28f5a-c633-fbfd-03cb-667427ab8e34>
2014-05-13 20:55:38 +0000

    Bắt đầu dự án

---
+++ trunk/ThuyetPhapApp/ThuyetPhapApp/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace ThuyetPhapApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.trvCategory = new System.Windows.Forms.TreeView();
+            this.imApp = new System.Windows.Forms.ImageList(this.components);
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.lvVideo = new System.Windows.Forms.ListView();
+            this.imVideo = new System.Windows.Forms.ImageList(this.components);
+            this.sfMovie = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sfMovie)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.trvCategory);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(916, 741);
+            this.splitContainer1.SplitterDistance = 304;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // trvCategory
+            // 
+            this.trvCategory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.trvCategory.ImageIndex = 0;
+            this.trvCategory.ImageList = this.imApp;
+            this.trvCategory.Location = new System.Drawing.Point(0, 0);
+            this.trvCategory.Name = "trvCategory";
+            this.trvCategory.SelectedImageIndex = 0;
+            this.trvCategory.Size = new System.Drawing.Size(304, 741);
+            this.trvCategory.TabIndex = 2;
+            this.trvCategory.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.trvCategory_NodeMouseClick);
+            // 
+            // imApp
+            // 
+            this.imApp.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imApp.ImageStream")));
+            this.imApp.TransparentColor = System.Drawing.Color.Transparent;
+            this.imApp.Images.SetKeyName(0, "Folder-icon.png");
+            this.imApp.Images.SetKeyName(1, "Folder-Open-icon.png");
+            this.imApp.Images.SetKeyName(2, "YouTube-icon.png");
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.lvVideo);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.sfMovie);
+            this.splitContainer2.Size = new System.Drawing.Size(608, 741);
+            this.splitContainer2.SplitterDistance = 377;
+            this.splitContainer2.TabIndex = 1;
+            // 
+            // lvVideo
+            // 
+            this.lvVideo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvVideo.LargeImageList = this.imVideo;
+            this.lvVideo.Location = new System.Drawing.Point(0, 0);
+            this.lvVideo.Name = "lvVideo";
+            this.lvVideo.Size = new System.Drawing.Size(608, 377);
+            this.lvVideo.SmallImageList = this.imVideo;
+            this.lvVideo.TabIndex = 1;
+            this.lvVideo.UseCompatibleStateImageBehavior = false;
+            this.lvVideo.SelectedIndexChanged += new System.EventHandler(this.lvVideo_SelectedIndexChanged);
+            this.lvVideo.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvVideo_MouseDoubleClick_1);
+            // 
+            // imVideo
+            // 
+            this.imVideo.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imVideo.ImageStream")));
+            this.imVideo.TransparentColor = System.Drawing.Color.Transparent;
+            this.imVideo.Images.SetKeyName(0, "YouTube-icon.png");
+            // 
+            // sfMovie
+            // 
+            this.sfMovie.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sfMovie.Enabled = true;
+            this.sfMovie.Location = new System.Drawing.Point(0, 0);
+            this.sfMovie.Name = "sfMovie";
+            this.sfMovie.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("sfMovie.OcxState")));
+            this.sfMovie.Size = new System.Drawing.Size(608, 360);
+            this.sfMovie.TabIndex = 0;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 741);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmMain";
+            this.Text = "Ứng dụng nghe Pháp";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sfMovie)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView trvCategory;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ListView lvVideo;
+        private AxShockwaveFlashObjects.AxShockwaveFlash sfMovie;
+        private System.Windows.Forms.ImageList imApp;
+        private System.Windows.Forms.ImageList imVideo;
+
+    }
+}
+
---
+++ trunk/ThuyetPhapApp/ThuyetPhapApp/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ThuyetPhapApp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            
+            //Khởi tạo các cột dữ liệu List View            
+            lvVideo.Columns.Add("Tên");
+            lvVideo.Columns.Add("Thời gian");
+            lvVideo.Columns.Add("Lượt xem");
+            lvVideo.View = System.Windows.Forms.View.LargeIcon;
+            
+            TreeNode N = new TreeNode("Chùa Long Viễn", 0, 1);
+                        
+            //Load dữ liệu từ file XML của Chùa Long Viễn
+            var docCat1 = XDocument.Load("http://chualongvien.com/model/load/CategoryBType/all");
+            var docCat2 = XDocument.Load("http://chualongvien.com/model/load/CategoryVideo/all");
+
+            //Thực hiện truy vấn để lấy dữ  liệu về
+            var qryCat1 = from c in docCat1.Root.Descendants("object")
+                              select c;
+
+            foreach (var obj in qryCat1)
+            {
+                string StrId = obj.Element("id").Value;
+                string StrName = obj.Element("name").Value;
+
+                //Thêm danh mục cha
+                TreeNode N1 = new TreeNode(StrName,0 , 1);
+                
+                //Thêm danh mục con
+                var qryCat2 = from c1 in docCat2.Root.Descendants("object")
+                              where (c1.Element("btype").Value == StrId) 
+                              select c1;
+
+                foreach (var obj1 in qryCat2)
+                {
+                    string StrId1   = obj1.Element("id").Value;
+                    string StrName1 = obj1.Element("name").Value;
+                    TreeNode N2 = new TreeNode(StrName1, 0, 1);
+                    N2.Tag = StrId1;
+                    N1.Nodes.Add(N2);
+                }
+                N.Nodes.Add(N1);
+            }
+            trvCategory.Nodes.Add(N);
+        }
+                
+        private void trvCategory_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            
+            //Khi nhấn vào mục cấp thứ 2 của cây dữ liệu thì mới xử lí
+            if (e.Node.Level == 2)
+            {
+                //Xóa sạch dữ liệu trước đó
+                lvVideo.Items.Clear();
+
+                //Nạp danh sách  Video Từ Website chualongvien.com
+                var docVideo = XDocument.Load("http://chualongvien.com/model/findby/VideoLibrary/" + e.Node.Tag);
+                var qryVideo = from c in docVideo.Root.Descendants("object")
+                                select c;
+
+                foreach (var obj in qryVideo)
+                {
+                    string StrId    = obj.Element("id").Value;
+                    string StrName  = obj.Element("name").Value;
+                    string StrURL   = obj.Element("url").Value;
+                    string StrTime  = obj.Element("time").Value;
+                    string StrCount = obj.Element("count").Value;
+
+                    var Video = new ListViewItem(new[] { StrName, StrTime, StrCount });
+                    Video.Tag = StrURL;
+                    Video.ImageIndex = 0;
+                    lvVideo.Items.Add(Video);
+                }
+                
+            }
+        }
+
+        private void lvVideo_ItemActivate(object sender, EventArgs e)
+        {
+            
+        }
+                
+        private void lvVideo_MouseDoubleClick_1(object sender, MouseEventArgs e)
+        {
+            ListView.SelectedListViewItemCollection items = lvVideo.SelectedItems;
+            MessageBox.Show(items[0].Tag.ToString());
+            sfMovie.Movie = items[0].Tag.ToString();
+        }
+
+        private void lvVideo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+}
